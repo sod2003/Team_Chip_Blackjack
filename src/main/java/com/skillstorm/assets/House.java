925a6cac321eea1f53d9e340770ac3e725ed4134
Joshua Holland <joshua.holland.1@outlook.com>
2024-01-26 14:41:58 -0500

    Added some properties and methods to House

--- src/main/java/com/skillstorm/assets/House.java
+++ src/main/java/com/skillstorm/assets/House.java
@@ -1,6 +1,84 @@
 package com.skillstorm.assets;
 
+import java.util.HashSet;
+
 // House (the Computer) Hand of cards Earnings Hit method Stay method etc.
 public class House {
-    
+
+    private String name;
+    private HashSet<Card> hand;
+    private double earnings;
+
+    public House() {
+    }
+
+    public House(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashSet<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(HashSet<Card> hand) {
+        this.hand = hand;
+    }
+
+    public double getEarnings() {
+        return earnings;
+    }
+
+    public void setEarnings(double earnings) {
+        this.earnings = earnings;
+    }
+
+    @Override
+    public String toString() {
+        return "House [name=" + name + ", hand=" + hand + ", earnings=" + earnings + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((hand == null) ? 0 : hand.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(earnings);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        House other = (House) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (hand == null) {
+            if (other.hand != null)
+                return false;
+        } else if (!hand.equals(other.hand))
+            return false;
+        if (Double.doubleToLongBits(earnings) != Double.doubleToLongBits(other.earnings))
+            return false;
+        return true;
+    }
+
 }
--- src/main/java/com/skillstorm/assets/Player.java
+++ src/main/java/com/skillstorm/assets/Player.java
@@ -1,13 +1,13 @@
 package com.skillstorm.assets;
 
-import java.util.List;
+import java.util.HashSet;
 
 // Player Name Earnings Hand of cards Hit method Split method Stay method etc.
 public class Player {
 
     private String name;
     private double earnings;
-    private List<Card> hand;
+    private HashSet<Card> hand;
 
     public Player(String name) {
         this.name = name;
@@ -34,11 +34,11 @@ public class Player {
         this.earnings = earnings;
     }
 
-    public List<Card> getHand() {
+    public HashSet<Card> getHand() {
         return hand;
     }
 
-    public void setHand(List<Card> hand) {
+    public void setHand(HashSet<Card> hand) {
         this.hand = hand;
     }
 
@@ -51,6 +51,47 @@ public class Player {
 
     public void stay() {
 
+    }
+
+    @Override
+    public String toString() {
+        return "Player " + name + ", earnings: " + earnings + ", hand: " + hand + ".";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(earnings);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((hand == null) ? 0 : hand.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Player other = (Player) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (Double.doubleToLongBits(earnings) != Double.doubleToLongBits(other.earnings))
+            return false;
+        if (hand == null) {
+            if (other.hand != null)
+                return false;
+        } else if (!hand.equals(other.hand))
+            return false;
+        return true;
     }
 
 }
