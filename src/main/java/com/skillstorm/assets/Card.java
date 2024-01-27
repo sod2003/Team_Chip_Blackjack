38fe8f66cbe9047fe720c6910ae1f6d2a9c8ff19
sod2003 <sod2003@gmail.com>
2024-01-27 17:14:14 -0500

    Adding state and properties to Deck and Card classes. Built Suit enum and TestDeck file.

--- src/main/java/com/skillstorm/assets/Card.java
+++ src/main/java/com/skillstorm/assets/Card.java
@@ -2,5 +2,60 @@ package com.skillstorm.assets;
 
 // The Card class, not specifically mentioned in the requirements, but it's a good design decision to delegate.
 public class Card {
-    
+    private Suit suit;
+    private int rank;
+
+    private Card(Suit suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public static Card[] generateCards() {
+        Card[] cards = new Card[52];
+        int index = 0;
+        for (Suit suit : Suit.values()) {
+            for (int i = 1; i <= 13; i++, index++) {
+                cards[index] = new Card(suit, i);
+            }
+        }
+        return cards;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+        result = prime * result + rank;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Card other = (Card) obj;
+        if (suit != other.suit)
+            return false;
+        if (rank != other.rank)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
--- src/main/java/com/skillstorm/assets/Deck.java
+++ src/main/java/com/skillstorm/assets/Deck.java
@@ -1,7 +1,58 @@
 package com.skillstorm.assets;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 // Deck All cards currently in the Deck Shuffle method Deal method Card Suit Value
 public class Deck {
+    private Queue<Card> cards;
+
+    public Deck(Card[] cards) {
+        this.cards = new LinkedList<>(Arrays.asList(cards));
+    }
+
+    public Queue<Card> getCards() {
+        return cards;
+    }
+
+    public Card draw() {
+        return cards.poll();
+    }
+
+    public void shuffle() {
+        Collections.shuffle((List<Card>) cards);
+    }
     
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cards == null) ? 0 : cards.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Deck other = (Deck) obj;
+        if (cards == null) {
+            if (other.cards != null)
+                return false;
+        } else if (!cards.equals(other.cards))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Deck [cards=" + cards + "]";
+    }
 }
