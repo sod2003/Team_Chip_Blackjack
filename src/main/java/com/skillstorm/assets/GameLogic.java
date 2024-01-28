c251595283b3b702396b24d703d1092abaad294a
Joshua Holland <joshua.holland.1@outlook.com>
2024-01-28 17:50:20 -0500

    Added a GameLogic class

---
+++ src/main/java/com/skillstorm/assets/GameLogic.java
@@ -0,0 +1,37 @@
+package com.skillstorm.assets;
+
+import java.util.LinkedList;
+
+/*
+ * A class for managing the overall logic of the game.
+ * The properties and methods within could potentially move to different classes if deemed appropriate.
+ */
+
+public class GameLogic {
+
+    private LinkedList<Player> playerList;
+    private House house;
+    private Deck deck;
+
+    /**
+     * Logic for dealing cards to everyone in the game including the house.
+     * Includes possibility of having multiple players.
+     */
+    public void deal() {
+        // Deal each player a card and set it to be face up
+        for (Player player : playerList) {
+            player.getHand().hit(deck);
+            player.getHand().getCardList().getFirst().setFaceUp(true);
+        }
+        // Deal house a card and set it to face up
+        house.getHand().hit(deck);
+        house.getHand().getCardList().getFirst().setFaceUp(true);
+        // Deal each player a second card but leave it face down
+        for (Player player : playerList) {
+            player.getHand().hit(deck);
+        }
+        // Deal second card to house but leave it face down
+        house.getHand().hit(deck);
+    }
+
+}
