fe4af4bf817e2a6f50c282bd585714b5298e6f1d
sod2003 <sod2003@gmail.com>
2024-02-04 00:21:39 -0500

    Creating mainMenu method in App.java.

--- src/main/java/com/skillstorm/App.java
+++ src/main/java/com/skillstorm/App.java
@@ -1,5 +1,7 @@
 package com.skillstorm;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -7,11 +9,33 @@ public final class App {
     private App() {
     }
 
+    static void mainMenu() {
+        boolean gameComplete = false;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Team Chip's Blackjack Game!\n");
+        while(!gameComplete) {
+            System.out.println("1. Login\n"
+            + "2. Play a game of Blackjack as a Guest\n"
+            + "3. See Leaderboard\n"
+            + "4. Exit Game\n");
+            String selection = sc.nextLine();
+            process(selection);
+        }
+        System.out.println("Thanks for playing Team Chip's Blackjack Game! Logging you out now...");
+        sc.close();
+    }
+
+    private static void process(String selection) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        mainMenu();
     }
 }
