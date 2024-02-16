49cdae2023bdd02d8e8d054d33f1fe867531e7d8
Joshua Holland <joshua.holland.1@outlook.com>
2024-02-16 12:37:30 -0500

    removed unused import from Load

--- src/main/java/com/skillstorm/logic/Load.java
+++ src/main/java/com/skillstorm/logic/Load.java
@@ -3,7 +3,6 @@ package com.skillstorm.logic;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import org.json.simple.JSONArray;
